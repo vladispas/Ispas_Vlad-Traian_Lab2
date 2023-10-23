b1e3d57414d65ad18f30604a873a3b7fec9c4025
Vlad Ispas <vlad.ispas@yahoo.com>
2023-10-23 14:24:38 +0300

    Fixed Create and Edit functionalities

--- Ispas_Vlad-Traian_Lab2/Models/Book.cs
+++ Ispas_Vlad-Traian_Lab2/Models/Book.cs
@@ -9,10 +9,10 @@ namespace Ispas_Vlad_Traian_Lab2.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        public int AuthorID { get; set; }
+        public int? AuthorID { get; set; }
         public decimal Price { get; set; }
 
-        public Author Author { get; set; }
-        public ICollection<Order> Orders { get; set; }
+        public Author? Author { get; set; }
+        public ICollection<Order>? Orders { get; set; }
     }
 }
